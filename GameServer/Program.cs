472d53f8dde16fb27025ea0c020d9470aeed68d1
Trevor Stubbs <stubbste@gmail.com>
2020-08-21 18:59:27 -0700

    initial commit

---
+++ GameServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game Server";
+
+            GameServer.Start(4, 26950);
+
+            Console.ReadKey();
+        }
+    }
+}
